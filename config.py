cd0ff101dd65e916f43ed88328552529f0e9a93e
Krup Kachhia <krup.k@scaledge.io>
2023-04-20 14:18:56 +0530

    Hyperparamters for model

---
+++ config.py
@@ -0,0 +1,45 @@
+IMG_SIZE = 224
+BATCH_SIZE = 64 
+ROOT_DIR = '101_ObjectCategories'
+
+Num_images   =  8677
+Shape_of_feature_list =  (8677, 2048)
+
+ABOUT_ME = """
+
+
+Greetings, and welcome to my webpage! 
+
+
+My name is Krup Kachhia, and I'm a skilled software engineer who is enrolled at the prestigious 
+CHARUSAT University to accompish my Bachelor in Technology degree in the domain of  Electronics 
+and Communication engineering. My insatiable appetite for programming drives me to 
+push the limits of what is possible, and I'm thrilled to be working as an intern in 
+the intriguing field of AI/ML at Scaledge Technolody, Ahmedabad.
+
+I've developed my expertise over the years in a variety of programming languages and 
+operating systems, including C++, Python, and Java. My natural curiosity and passion of learning 
+have always put me ahead of the curve, and I'm looking forward to taking on new challenges and 
+expanding my horizons.
+
+Chess and cricket are my two favourite activities to play when I'm not coding since they both 
+help me stay focused and clear-headed. Leading a team to create a humanoid robot was one of the 
+highlights of my time in college as it allowed me to apply my programming expertise to a 
+real-world project and collaborate with people to achieve a shared objective.
+
+Reverse Image Search, my most recent project, enables users to search for photos based on the 
+content of individual images by utilising cutting-edge AI and machine learning algorithms. 
+How we engage with photographs on the internet is being changed by the potential 
+to instantaneously choose aesthetically pleasant images rather than dependent on 
+text-based search queries.
+
+Reverse image search may be used for a variety of things, 
+such as making it easier to identify and validate images found in web content 
+and speeding up the search for items in e-commerce. I'm excited to be able to share my 
+concept with you, and I strongly encourage you to get in touch with me if you have 
+any inquiries or would like to find out more information regarding reverse image search. 
+
+
+I appreciate you visiting my website!
+
+"""
