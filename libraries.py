7a7fbebcfd9b8906bcdeda8e8d83d78a5e9833f6
Krup Kachhia <krup.k@scaledge.io>
2023-04-20 14:20:04 +0530

    Contains libraries needed for project

---
+++ libraries.py
@@ -0,0 +1,104 @@
+# Import the os library for operating system specific functionality
+import os
+
+
+
+# Mathematical Functions
+    # Import the math library for mathematical operations
+import math                  
+
+    # Import the numpy library and alias it as 'np' for numerical operations
+import numpy as np
+
+    # Import the norm function from the numpy.linalg library for computing the vector norm
+from numpy.linalg import norm  
+
+
+
+# Image Manipulation
+    # Import the Python Imaging Library (PIL) for image manipulation
+import PIL                   
+
+    # Import the Image module from PIL for working with images
+from PIL import Image        
+
+
+
+# Import the requests library for sending HTTP requests
+import requests
+
+
+
+# Garbage Collection
+    # Import the garbage collection module for freeing up memory
+import gc                    
+
+
+
+# Progress Bars
+    # Import the tqdm module for displaying progress bars during long-running computations
+from tqdm import tqdm        
+
+
+
+# Parallel Computing and Caching
+    # Import the joblib library and alias it as 'pickle' for parallel computing and caching
+import joblib as pickle      
+
+
+
+# Data Visualization
+    # Import the pyplot module from the matplotlib library and alias it as 'plt' for data visualization
+import matplotlib.pyplot as plt  
+
+    # Import the image module from the matplotlib library and alias it as 'mpimg' for working with images
+import matplotlib.image as mpimg 
+
+
+
+# Unsupervised Learning
+    # Import the NearestNeighbors class from the scikit-learn library for unsupervised learning
+from sklearn.neighbors import NearestNeighbors  
+
+
+
+# Principal Component Analysis
+    # Import the PCA class from the scikit-learn library for principal component analysis
+from sklearn.decomposition import PCA          
+
+
+
+# Streamlit and Lottie
+    # Import the Streamlit library and alias it as 'st' for building web applications
+import streamlit as st          
+
+    # Import the command function from the click library for building command line applications
+from click import command       
+
+    # Import the st_lottie function from the streamlit_lottie library for rendering Lottie animations
+from streamlit_lottie import st_lottie   
+
+
+
+# Deep Learning and Image Processing
+    # Import the TensorFlow library and alias it as 'tf' for deep learning and image processing
+import tensorflow as tf    
+
+    # Import the image module from the TensorFlow library for working with images
+from tensorflow.keras.preprocessing import image  
+
+    # Import the ImageDataGenerator class from the TensorFlow library for image augmentation
+from tensorflow.keras.preprocessing.image import ImageDataGenerator  
+
+    # Import the ResNet50 model and the preprocess_input function from the TensorFlow library for image classification
+from tensorflow.keras.applications.resnet50 import ResNet50, preprocess_input
+
+
+
+# Import the time module for time-related operations
+import time
+
+
+
+# Import the BytesIO class from the io library for working with bytes objects
+from io import BytesIO
